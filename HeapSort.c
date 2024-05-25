7f0924cbb3de8407c94354f56b27ed948c8ad2b0
Edward <78650387+WangEdward1027@users.noreply.github.com>
2024-05-25 15:30:42 +0800

    堆排序

--- HeapSort.c
+++ HeapSort.c
@@ -16,11 +16,11 @@ void print_array(int arr[], int n) {
 	printf("\n");
 }
 
-//i：需要调整的结点索引
-//n：索引的最大范围
+//3.堆化
+//i：需要调整的结点索引    n：索引的最大范围
 void heapify(int arr[], int i, int n) {
 	while (i < n) {
-		//求三个元素的最大值
+		//求根、左、右 三个元素的最大值
 		int lchild = 2 * i + 1;
 		int rchild = 2 * i + 2;
 		int maxIdx = i;
@@ -30,20 +30,25 @@ void heapify(int arr[], int i, int n) {
 		if (rchild < n && arr[rchild] > arr[maxIdx]) {
 			maxIdx = rchild;
 		}
-		if (maxIdx == i)	break;  //如果最大值是根结点，调整提前结束  
-		SWAP(arr, i, maxIdx);		//如果不是，交换根结点和最大值结点
-		i = maxIdx;					//继续下坠
+		//如果最大值是根结点，调整提前结束
+		if (maxIdx == i)	break;  
+		//如果不是，交换根结点和最大值结点
+		SWAP(arr, i, maxIdx);	
+		//调整maxIdx结点，继续下坠
+		i = maxIdx;					
 	} //i >= n || maxIdx == i
 }
 
+//2.构建大顶堆
 void build_heap(int arr[], int n) {
 	//找到第一个非叶子结点i: lchild(i) = 2i+1 < n，2i+1 <= n-1, 2i <= n-2， i <= n/2 - 1
 	//从后往前依次构建大顶堆
-	for (int i = n / 2 - 1; i >= 0; i--) {
+	for (int i = n/2-1; i >= 0; i--) {
 		heapify(arr, i, n);
 	}
 }
 
+//1.堆排序
 void heap_sort(int arr[], int n) {
 	//1.构建大顶堆
 	build_heap(arr, n);
@@ -53,12 +58,12 @@ void heap_sort(int arr[], int n) {
 	while (len > 1) {
 		SWAP(arr, 0, len-1);
 		len--;
-		heapify(arr, 0, len);
+		heapify(arr, 0, len);	//堆化
 	}
 }
 
 int main(void) {
-	int arr[] = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+	int arr[] = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
 	heap_sort(arr, 10);
 	print_array(arr, 10);
 
