b419c701831a04f8813d28288fcbe266c0db4880
Edward <78650387+WangEdward1027@users.noreply.github.com>
2024-05-25 15:01:49 +0800

    Create SelectionSort.c

---
+++ SelectionSort.c
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#define SIZE(a) (sizeof(a)/sizeof(a[0]))
+
+//选择排序
+
+void swap(int* a, int* b) {
+	int temp = *b;
+	*b = *a;
+	*a = temp;
+}
+
+void SelectionSort(int A[], int n) {
+	for (int i = 0; i < n - 1; i++) {  //n-1趟
+		int minIdx = i;
+		//寻找最小值的下标
+		for (int j = i + 1; j < n; j++) {
+			if (A[j] < A[minIdx])  minIdx = j;
+		}
+		//交换
+		if (minIdx != i) {
+			swap(&A[i], &A[minIdx]);
+		}
+	}
+}
+
+int main(void) {
+	int arr[] = { 5,4,3,2,1 };
+	SelectionSort(arr, SIZE(arr));
+
+	for (int i = 0; i < SIZE(arr); i++) {
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+	return 0;
+}
